d97d84a3766f84dc2d0d251334f3967f9b8f72c0
Liz R <lizr588@gmail.com>
2018-07-24 08:22:23 -0500

    first

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,46 @@
+class WelcomeController < ApplicationController
+  def test
+    response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?zip=28204&units=imperial&appid=#{ENV['openweather_api_key']}")
+    @location = response['name']
+    @temp = response['main']['temp']
+    @weather_icon = response['weather'][0]['icon']
+    @weather_words = response['weather'][0]['description']
+    @cloudiness = response['clouds']['all']
+    @windiness = response['wind']['speed']
+
+#alternate way to store the data (as a hash)
+    # response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?zip=
+    #   75001&units=imperial&appid=#{ENV['openweather_api_key']}")
+    #
+    # @results = {}
+    # @results[:location] = response['name']
+    # @results[:temp] = response['main']['temp']
+    # @results[:weather_icon] = response['weather'][0]['icon']
+    # @results[:weather_words] = response['weather'][0]['description']
+    # @results[:windiness] = response['wind']['speed']
+    # @results[:cloudiness] = response['clouds']['all']
+
+  end
+
+def index
+    # checks that the state and city params are not empty before calling the API
+    if params[:zipcode] != '' && !params[:zipcode].nil?
+      response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?zip=#{params[:zipcode]}
+&units=imperial&appid=#{ENV['openweather_api_key']}")
+      @status = response['cod']
+      # if no error is returned from the call, we fill our instance variables with the result of the call
+      if @status != '404' && response['message'] != 'city not found'
+        @location = response['name']
+        @temp = response['main']['temp']
+        @weather_icon = response['weather'][0]['icon']
+        @weather_words = response['weather'][0]['description']
+        @cloudiness = response['clouds']['all']
+        @windiness = response['wind']['speed']
+      else
+        # if there is an error, we report it to our user via the @error variable
+        @error = response['message']
+
+    end
+  end
+end
+end
